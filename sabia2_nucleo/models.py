5a2c12b9b9c203ad1de54b12c3ef92d35dcdb0e6
Marlon Ferrari <mfmarlonferrari@gmail.com>
2014-06-16 12:17:25 -0300

    login adicionado

--- sabia2_nucleo/models.py
+++ sabia2_nucleo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Artigo(models.Model):
+    titulo = models.CharField(max_length=100)
+    conteudo = models.TextField()
+    resumo = models.CharField(max_length=100)
+    tags = models.CharField(max_length=50)
