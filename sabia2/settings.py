6164fd74448c8a70620d26c088f7ab165e2a9ca4
Marlon Ferrari <mfmarlonferrari@gmail.com>
2014-07-27 10:09:47 -0300

    parte  do Marlon integrada - sistema de tags automáticas com base no títiulo do documento e pesquisa de documentos relacionados na web pelas tags.

--- sabia2/settings.py
+++ sabia2/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Sabia',
+    'taggit',
+    'googlesearch',
 )
 
 MIDDLEWARE_CLASSES = (
--- sabia2/urls.py
+++ sabia2/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Ufes.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'Sabia.views.index', name='index'),    
 	url(r'^autenticar_usuario/$', 'Sabia.views.autenticar_usuario', name='autenticar_usuario' ),
     
@@ -16,4 +13,6 @@ urlpatterns = patterns('',
     url(r'^documento/$', 'Sabia.views.documento', name='documento' ), # pagina de cadastro
     url(r'^salvar_documento/$', 'Sabia.views.salvar_documento', name='salvar_documento' ), # pagina de cadastro
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^pesquisa/', include('googlesearch.urls')),
+    url(r'^relacionados/', 'Sabia.views.relacionados', name='relacionados'),
 )
